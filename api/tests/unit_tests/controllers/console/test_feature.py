188f970c99a0413c1d931fe66d9c5d8392bf2f19
Byron Wang <byron@dify.ai>
2026-08-10 21:25:16 +0800

    test(api): mock account service bundle

--- api/tests/unit_tests/controllers/console/test_feature.py
+++ api/tests/unit_tests/controllers/console/test_feature.py
@@ -6,7 +6,6 @@ from pytest_mock import MockerFixture
 from enums.deployment_edition import DeploymentEdition
 from extensions.ext_application_services import AccountServices, ApplicationServices
 from machinery.context import RequestContext
-from services.account_profile_service import AccountProfileService
 from services.entities.feature_entities import (
     FeatureModel,
     LicenseLimitationModel,
@@ -36,9 +35,7 @@ def _request_context() -> RequestContext:
 def _install_application_services(mocker: MockerFixture):
     feature_queries = create_autospec(FeatureQueryService, instance=True, spec_set=True)
     services = ApplicationServices(
-        accounts=AccountServices(
-            profile=create_autospec(AccountProfileService, instance=True, spec_set=True),
-        ),
+        accounts=create_autospec(AccountServices, instance=True, spec_set=True),
         explore_banner_queries=create_autospec(ExploreBannerQueryService, instance=True, spec_set=True),
         schema_definitions=create_autospec(SchemaDefinitionService, instance=True, spec_set=True),
         setup=create_autospec(SetupService, instance=True, spec_set=True),
